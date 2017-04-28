0fcb79e7959d7de73d74e8ffec76147d64443086
dpower1 <dpower1@umbc.edu>
2017-04-28 14:20:18 -0400

    Finishing up profile stuff

--- edit.php
+++ edit.php
@@ -8,7 +8,8 @@
 <body>
 
 <form method="POST" name="post" action="new_userinfo.php" enctype="multipart/form-data"> 
-	<textarea name="body" placeholder="Enter your user profile here" ></textarea><br />
+	Name<input type="text" name="name" /><br />
+	<textarea name="body" placeholder="Enter your user description here" ></textarea><br />
 	Location<input type="text" name="location" /><br />
 	Select Image to upload<input type="file" name="fileToUpload" id="fileToUpload"><br />
 	<input type="submit" value="Submit" />
