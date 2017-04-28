dbce993ef1e32650d55c406b86f81b9c69a0433a
root <root@ip-172-31-30-133.ec2.internal>
2017-04-28 19:02:30 +0000

    changed layout

--- match/match.php
+++ match/match.php
@@ -1,12 +1,25 @@
 <!DOCTYPE html>
 <html>
+<head>
+  <script src="https://www.w3schools.com/lib/w3data.js"></script>
+  <div w3-include-html="/dog4dog/header.html"></div>
+  <script>
+    w3IncludeHTML();
+  </script>
+  
+  <style type="text/css">
+  #matches {
+    position: absolute;
+    width: 300px;
+    height: 200px;
+    z-index: 15;
+    top: 50%;
+    left: 50%;
+    margin: -100px 0 0 -150px;
+  }
+  </style>
 
-<script src="https://www.w3schools.com/lib/w3data.js"></script>
-<div w3-include-html="/dog4dog/header.html"></div>
-<script>
-w3IncludeHTML();
-</script>
-
+</head>
 
 
 
@@ -23,17 +36,19 @@ $row = mysqli_fetch_array($result);
 $matches = $row['Matched'];
 
 //echo "YOUR MATCHES: " . $matches;
-
+echo "<div id=\"matches\">";
 foreach (explode(",", $matches) as $match) {
   echo "\r\nMatch: " . $match;
-  echo "<button class=\"m-button muut-messaging-button\" data-recipients=\"Display Name <jrust>; Another name <sadieb1>\" data-forumname=\"dog4dog\">Message username and username2</button>
+  echo "<br><button class=\"m-button muut-messaging-button\" data-recipients=\"dog <sadieb1>\" data-forumname=\"dog4dog\">Message dog</button>
 <script src=\"//cdn.muut.com/1/moot.min.js\"></script>";
 }
-
+echo "</div>";
 ?>
-
-<iframe src="muut.htm" height="400" width="600">
-</iframe>
+<!--
+  echo "<br><button class=\"m-button muut-messaging-button\" data-recipients=\"Display Name <jrust>; Another name <sadieb1>\" data-forumname=\"dog4dog\">Message username and username2</button> 
+<script src=\"//cdn.muut.com/1/moot.min.js\"></script>"; -->
+<!--<iframe src="muut.htm" height="400" width="600">
+</iframe> -->
 
 </body>
 </html>
